035a9cfadbd8273df4066671be29bf70ef0d4153
mpoemsl <37959974+mpoemsl@users.noreply.github.com>
2023-03-17 12:31:38 +0100

    Update custom images to new branch naming

--- docker/build_custom_images.sh
+++ docker/build_custom_images.sh
@@ -1,19 +1,7 @@
 #!/bin/sh
 
-# build 0.7.1-cpu
-./build_image.sh -bt dev -b  torchserve-0.7.1 -t textshuttle/pytorch-serve:0.7.1-cpu
+# build 23mt-cpu
+./build_image.sh -bt dev -b torchserve-23mt -t textshuttle/pytorch-serve:23mt-cpu
 
-# build 0.7.1-gpu
-./build_image.sh -bt dev -g -cv cu113 -b torchserve-0.7.1 -t textshuttle/pytorch-serve:0.7.1-gpu
-
-# build prio-cpu
-./build_image.sh -bt dev -b feature/priority-linked-blocking-deque -t textshuttle/pytorch-serve:prio-cpu
-
-# build prio-gpu
-./build_image.sh -bt dev -g -cv cu113 -b feature/priority-linked-blocking-deque -t textshuttle/pytorch-serve:prio-gpu
-
-# build prio-qt-cpu
-./build_image.sh -bt dev -b feature/queue-timeout -t textshuttle/pytorch-serve:prio-qt-cpu
-
-# build prio-qt-gpu
-./build_image.sh -bt dev -g -cv cu113 -b feature/queue-timeout -t textshuttle/pytorch-serve:prio-qt-gpu
+# build 23mt-gpu
+./build_image.sh -bt dev -g -cv cu113 -b torchserve-23mt -t textshuttle/pytorch-serve:23mt-gpu
