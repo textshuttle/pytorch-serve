811fed743b2ee441f660850131bf63e13c904794
mpoemsl <37959974+mpoemsl@users.noreply.github.com>
2023-03-20 12:10:48 +0100

    Implement random selection weighted by free memory

--- frontend/server/src/main/java/org/pytorch/serve/util/GPUManager.java
+++ frontend/server/src/main/java/org/pytorch/serve/util/GPUManager.java
@@ -1,8 +1,12 @@
 package org.pytorch.serve.util;
 
 import java.util.Map;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.TreeMap;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.nio.charset.StandardCharsets;
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -19,14 +23,21 @@ public final class GPUManager {
     private static GPUManager instance;
 
     private int nGPUs;
+    private int minFreeMemory;
+    private float maxShareFailures;
     private AtomicInteger[] nFailures;
     private AtomicInteger[] freeMemory;
     private ConcurrentHashMap<String, Integer> workerIds;
 
     private GPUManager(int nGPUs) {
         this.nGPUs = nGPUs;
-        this.workerIds = new ConcurrentHashMap<String, Integer>();
+
+        // TODO pass these through as parameters
+        this.minFreeMemory = 100;
+        this.maxShareFailures = 0.5f;
+
         if (nGPUs > 0) {
+            this.workerIds = new ConcurrentHashMap<String, Integer> ();
             this.nFailures = new AtomicInteger[this.nGPUs];
             this.freeMemory = new AtomicInteger[this.nGPUs];
             for (int i = 0; i < this.nGPUs; i++) {
@@ -80,9 +91,10 @@ public final class GPUManager {
     }
 
     public int getGPU(String workerId) {
+        int gpuId = -1;
         // return -1 if there are no gpus
         if (this.nGPUs == 0) {
-            return -1;
+            return gpuId;
         }
         // if the worker was previously assigned to a GPU and now requests a new one, it has likely failed
         // increment failure counter for the previously assigned GPU
@@ -93,9 +105,44 @@ public final class GPUManager {
         for (int i = 0; i < this.nGPUs; i++) {
             this.freeMemory[i].set(queryNvidiaSmiFreeMemory(i));
         }
-        // assign GPU id (dummy for now)
-        int gpuId = 0;
-        logger.info("Assigning gpuId " + String.valueOf(gpuId) + " to workerId " + workerId);
+        // get total failures for share calculation
+        int nFailuresSum = 0;
+        for (int i = 0; i < this.nGPUs; i++) {
+            nFailuresSum += this.nFailures[i].intValue();
+        }
+        // get free memory for all eligible GPUs
+        HashMap<Integer, Integer> eligibleIdFreeMems = new HashMap<Integer, Integer> ();
+        for (int i = 0; i < this.nGPUs; i++) {
+            // check that free memory is available and exceeds minimum
+            if (this.freeMemory[i].intValue() > this.minFreeMemory) {
+                // check that share of failures is smaller than maximum
+                float shareFailures = (float) this.nFailures[i].intValue() / (float) nFailuresSum;
+                if (shareFailures < this.maxShareFailures) {
+                    eligibleIdFreeMems.put(i, this.freeMemory[i].intValue());
+                }
+            }
+        }
+        // get sum of eligible id free memory for prob calculation
+        int eligibleIdFreeMemSum = 0;
+        for (Map.Entry<Integer, Integer> entry : eligibleIdFreeMems.entrySet()) {
+            eligibleIdFreeMemSum += entry.getValue();
+        }
+        // store cumulative probabilities in navigable map
+        float cumProb = 0.0f;
+        TreeMap<Float, Integer> cumProbIds = new TreeMap<Float, Integer> ();
+        for (Map.Entry<Integer, Integer> entry : eligibleIdFreeMems.entrySet()) {
+            int i = entry.getKey();
+            int freeMem = entry.getValue();
+            cumProbIds.put(cumProb, i);
+            cumProb += (float) freeMem / (float) eligibleIdFreeMemSum;
+        }
+        // make random selection
+        float randFloat = ThreadLocalRandom.current().nextFloat();
+        gpuId = cumProbIds.ceilingEntry(randFloat).getValue();
+        logger.info("Assigning gpuId " + String.valueOf(gpuId) + 
+                    " with free memory " + String.valueOf(eligibleIdFreeMems.get(gpuId)) + 
+                    " with number of failures " + String.valueOf(this.nFailures[gpuId].intValue()) + 
+                    " to workerId " + workerId);
         return gpuId;
     }
 }
