624f1080cfa84fc34988cee4439fed2f09a9e204
Martin Pömsl <37959974+mpoemsl@users.noreply.github.com>
2023-03-22 12:51:10 +0100

    Remove unused import
    Co-authored-by: Simon Schölly <sischoel@gmail.com>

--- frontend/server/src/main/java/org/pytorch/serve/util/GPUManager.java
+++ frontend/server/src/main/java/org/pytorch/serve/util/GPUManager.java
@@ -1,7 +1,6 @@
 package org.pytorch.serve.util;
 
 import java.util.Map;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.TreeMap;
 import java.util.concurrent.ConcurrentHashMap;
