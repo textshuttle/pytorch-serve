08559c78ec0029bf5931889e44e6f672e912ff57
Martin Pömsl <poemsl@erinome>
2024-03-04 11:04:23 +0100

    Switch back to Dockerfile.dev building

--- docker/build_custom_images.sh
+++ docker/build_custom_images.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
+# increment when appropriate
+VERSION=2
+
 # build cpu
-./build_image.sh -t textshuttle/pytorch-serve:torchserve-23mt-v0.8.0-v2-cpu
+./build_image.sh -bt dev -b torchserve-23mt-v0.8.0 -t textshuttle/pytorch-serve:torchserve-23mt-v0.8.0-v${VERSION}-cpu
 
 # build gpu
-./build_image.sh -g -cv cu118 -t textshuttle/pytorch-serve:torchserve-23mt-v0.8.0-v2-gpu
+./build_image.sh -bt dev -b torchserve-23mt-v0.8.0 -g -cv cu118 -t textshuttle/pytorch-serve:torchserve-23mt-v0.8.0-v${VERSION}-gpu
+
+# note that this will build arm/amd images depending on your OS
+# for multi-platform building and tagging, see https://github.com/textshuttle/pytorch-serve/pull/15#issuecomment-1906360733
